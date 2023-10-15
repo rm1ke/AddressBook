0bf4610b3a5bc56cd57841adfe040317973d72bc
rm1ke <mikeriecker@gmail.com>
2023-10-14 23:03:58 -0700

    Add project files.

---
+++ AddressBook.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace TheAddressBook
+{
+    public class AddressBook
+    {
+        private List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        private readonly IConfiguration _configuration;
+
+        public AddressBook(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+        
+        public void AddContact(Contact contact)
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Contacts (FirstName, LastName, Birthday) VALUES (@FirstName, @LastName, @Birthday)";
+                if (connectionString == null)
+                {
+                    Console.WriteLine("Connection string not found. Please check your configuration.");
+                }
+                else {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@FirstName", contact.FirstName);
+                        command.Parameters.AddWithValue("@LastName", contact.LastName);
+                        command.Parameters.AddWithValue("@Birthday", contact.Birthday);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        // methods for search, update, delete will be added here
+        public void DisplayContacts()
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT FirstName, LastName, Birthday FROM Contacts";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["FirstName"]} {reader["LastName"]}, Birthday: {Convert.ToDateTime(reader["Birthday"]).ToString("yyyy-MM-dd")}");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SearchContacts(string query)
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT FirstName, LastName, Birthday FROM Contacts WHERE FirstName LIKE @Query OR LastName LIKE @Query";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    // Adding a '%' wildcard to the query to match any characters before or after the query text
+                    command.Parameters.AddWithValue("@Query", "%" + query + "%");
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("No contacts found.");
+                            return;
+                        }
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["FirstName"]} {reader["LastName"]}, Birthday: {Convert.ToDateTime(reader["Birthday"]).ToString("yyyy-MM-dd")}");
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void SearchContactsByMonth(int month)
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT FirstName, LastName, Birthday FROM Contacts WHERE MONTH(Birthday) = @Month";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Month", month);
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("No contacts found.");
+                            return;
+                        }
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["FirstName"]} {reader["LastName"]}, Birthday: {Convert.ToDateTime(reader["Birthday"]).ToString("yyyy-MM-dd")}");
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+        public void UpdateContact(string firstName, string lastName, Contact updatedContact)
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Contacts SET FirstName = @NewFirstName, LastName = @NewLastName, Birthday = @NewBirthday WHERE FirstName = @FirstName AND LastName = @LastName";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    command.Parameters.AddWithValue("@NewFirstName", updatedContact.FirstName);
+                    command.Parameters.AddWithValue("@NewLastName", updatedContact.LastName);
+                    command.Parameters.AddWithValue("@NewBirthday", updatedContact.Birthday);
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected == 0)
+                    {
+                        Console.WriteLine("No contact found to update.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact updated successfully.");
+                    }
+                }
+            }
+        }
+
+
+        public void DeleteContact(string firstName, string lastName)
+        {
+            string connectionString = _configuration.GetConnectionString("AddressBookDB");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Contacts WHERE FirstName = @FirstName AND LastName = @LastName";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", firstName);
+                    command.Parameters.AddWithValue("@LastName", lastName);
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Contact deleted successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No contact found with the given name.");
+                    }
+                }
+            }
+        }
+
+
+
+        //public void TestDatabaseConnection()
+        //{
+        //    string connectionString = _configuration.GetConnectionString("AddressBookDB");
+        //    try
+        //    {
+        //        using (SqlConnection connection = new SqlConnection(connectionString))
+        //        {
+        //            connection.Open();
+        //            Console.WriteLine("Database connection successful.");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Database connection failed: " + ex.Message);
+        //    }
+        //}
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using TheAddressBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.FileExtentions;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Data.SqlClient;
+using System.IO;
+
+
+namespace TheAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+
+            AddressBook addressBook = new AddressBook(configuration);
+            //Console.WriteLine(configuration.GetConnectionString("AddressBookDB"));
+            //addressBook.TestDatabaseConnection();
+            while (true) // this creates an infinite loop that will keep displaying the menu
+            {
+                Console.Clear(); // clears the console screen - keeps menu tidy
+                Console.WriteLine("1. Add a new contact");
+                Console.WriteLine("2. View all contacts");
+                Console.WriteLine("3. Search for a contact");
+                Console.WriteLine("4. Search by month");
+                Console.WriteLine("5. Update a contact");
+                Console.WriteLine("6. Delete a contact");
+                Console.WriteLine("7. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter first name: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Enter Last name: ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Enter birthday (yyyy-mm-dd): ");
+                        DateTime birthday;
+                        while (!DateTime.TryParse(Console.ReadLine(), out birthday))
+                        {
+                            Console.Write("Invalid date, please enter again (yyyy-mm-dd): ");
+                        }
+                        Contact newContact = new Contact
+                        {
+                            FirstName = firstName,
+                            LastName = lastName,
+                            Birthday = birthday
+                        };
+                        addressBook.AddContact(newContact);
+                        Console.WriteLine("Contact added successfully.");
+                        break;
+                    case "2":
+                        addressBook.DisplayContacts();
+                        break;
+                    case "3":
+                        Console.Write("Enter name to search: ");
+                        string query = Console.ReadLine();
+                        addressBook.SearchContacts(query);
+                        break;
+                    case "4":
+                        Console.Write("Enter month (01-12): ");
+                        string monthInput = Console.ReadLine();
+                        int month;
+                        while (!int.TryParse(monthInput, out month) || month < 1 || month > 12)
+                        {
+                            Console.Write("Invalid month, please enter again (01-12): ");
+                            monthInput = Console.ReadLine();
+                        }
+                        addressBook.SearchContactsByMonth(month);
+                        break;
+                    case "5":
+                        Console.Write("Enter the first name of the contact you want to update: ");
+                        string oldFirstName = Console.ReadLine();
+                        Console.Write("Enter the last name of the contact you want to update: ");
+                        string oldLastName = Console.ReadLine();
+                        Console.Write("Enter new first name: ");
+                        string updateFirstName = Console.ReadLine();
+                        Console.Write("Enter new last name: ");
+                        string updateLastName = Console.ReadLine();
+                        Console.Write("Enter new birthday (yyyy-mm-dd): ");
+                        DateTime updateBirthday;
+                        string birthdayInput = Console.ReadLine();
+                        while (!DateTime.TryParse(birthdayInput, out updateBirthday))
+                        {
+                            Console.Write("Invalid date, please enter again (yyyy-mm-dd): ");
+                            birthdayInput = Console.ReadLine();
+                        }
+                        Contact updatedContact = new Contact
+                        {
+                            FirstName = updateFirstName,
+                            LastName = updateLastName,
+                            Birthday = updateBirthday
+                        };
+                        addressBook.UpdateContact(oldFirstName, oldLastName, updatedContact);
+                        break;
+                    case "6":
+                        Console.Write("Enter first name: ");
+                        string deleteFirstName = Console.ReadLine();
+                        Console.Write("Enter last name: ");
+                        string deleteLastName = Console.ReadLine();
+                        addressBook.DeleteContact(deleteFirstName, deleteLastName);
+                        break;
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+
+            // additional code for user interaction here
+        }
+    }
+}
